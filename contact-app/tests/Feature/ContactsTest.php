355e1f01e8a79899aa2d2aea0c096690bec35271
Neibue <devwithmax@outlook.com>
2020-04-02 11:12:08 +0200

    Adding Database refreshing
    Adding a guard to the Contact model to detect more errors on test suite

--- contact-app/tests/Feature/ContactsTest.php
+++ contact-app/tests/Feature/ContactsTest.php
@@ -10,6 +10,8 @@ use Tests\TestCase;
 
 class ContactsTest extends TestCase
 {
+    use RefreshDatabase;
+
     /**
      * A basic feature test example.
      *
@@ -19,6 +21,8 @@ class ContactsTest extends TestCase
      */
     public function a_contact_can_be_added()
     {
+        $this->withoutExceptionHandling();
+
         $this->post('/api/contacts', ['name' => 'Test Name']);
 
         $this->assertCount(1, Contact::all());
