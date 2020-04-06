34664b7878614f5fcdf7ca3e39a727840b5fc914
Neibue <devwithmax@outlook.com>
2020-04-06 09:41:10 +0200

    Adding contact policy and contact resource
    Adding a contact policy to manage the access of the data. 
    Refactoring and adding a contact resource to simplify the way we return data from the database.

--- contact-app/app/Http/Controllers/ContactsController.php
+++ contact-app/app/Http/Controllers/ContactsController.php
@@ -4,32 +4,33 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Contact;
+use App\Http\Resources\Contact as ResourcesContact;
 
 class ContactsController extends Controller
 {
     public function index()
     {
-        return request()->user()->contacts;
+        $this->authorize('viewAny', Contact::class);
+
+        return ResourcesContact::collection(request()->user()->contacts);
     }
 
     public function store()
     {
+        $this->authorize('create', Contact::class);
         request()->user()->contacts()->create($this->validateData());
     }
 
     public function show(Contact $contact)
     {
-        if (request()->user()->isNot($contact->user)) {
-            return response([], 403);
-        }
-        return $contact;
+        $this->authorize('view', $contact);
+
+        return new ResourcesContact($contact);
     }
 
     public function update(Contact $contact)
     {
-        if (request()->user()->isNot($contact->user)) {
-            return response([], 403);
-        }
+        $this->authorize('update', $contact);
 
         $contact->update($this->validateData());
     }
@@ -46,9 +47,8 @@ class ContactsController extends Controller
 
     public function destroy(Contact $contact)
     {
-        if (request()->user()->isNot($contact->user)) {
-            return response([], 403);
-        }
+        $this->authorize('delete', $contact);
+
         $contact->delete();
     }
 }
